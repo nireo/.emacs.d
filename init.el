1a10b36735cfbef280db56bef9ae53bf08a6d962
nireo <eemil.lehtikangas1@gmail.com>
2021-01-07 01:11:41 +0200

    added some lsp things

--- init.el
+++ init.el
@@ -11,15 +11,18 @@
 (setq visible-bell t) ;; Remove the beep
 
 ;; Set the font more readable
-(set-face-attribute 'default nil :font "Source Code Pro" :height 150)
+(set-face-attribute 'default nil :font "Source Code Pro" :height 160)
 
 ;; Load the emacs theme
 (load-theme 'wombat)
 
+;; Set line numbers
+(column-number-mode)
+(global-display-line-numbers-mode t)
+
 (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
 
 (require 'package)
-
 (setq package-archives '(("melpa" . "https://melpa.org/packages/")
                          ("org" . "https://orgmode.org/elpa/")
                          ("elpa" . "https://elpa.gnu.org/packages/")))
@@ -36,8 +39,6 @@
 (setq use-package-always-ensure t)
 
 (use-package counsel)
-
-
 
 ;; Download Evil
 (unless (package-installed-p 'evil)
@@ -65,9 +66,32 @@
   :config
   (ivy-mode 1))
 
+(use-package doom-modeline
+  :ensure t
+  :init (doom-modeline-mode 1)
+  :custom ((doom-modeline-height 15))
+
 (use-package rust-mode)
 (setq rust-format-on-save t)
 
+(use-package lsp-mode
+  :commands (lsp lsp-deferred)
+  :init
+  (setq lsp-keyma-prefix "C-c l")
+  :config
+  (lsp-enable-which-key-integration t))
+
+(setq lsp-prefer-capf t)
+(setq lsp-completion-provider :capf)
+(setq lsp-completion-enable t)
+
+(use-package lsp-ui-mode)
+
+(use-package typescript-mode
+  :mode "\\.ts\\'"
+  :hook (typescript-mode . lsp-deferred)
+  :config
+  (setq typescript-indent-level 2))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -81,4 +105,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("18cd5a0173772cdaee5522b79c444acbc85f9a06055ec54bb91491173bc90aaa" default))
  '(package-selected-packages '(counsel ivy use-package)))
