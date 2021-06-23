b87fbfe740ed84606fae5a421c38d79b1d1b9d69
nireo <eemil.lehtikangas1@gmail.com>
2021-06-23 19:42:12 +0300

    added some stuff to org mode like spell checking

--- init.el
+++ init.el
@@ -7,43 +7,43 @@
 
 ;; Use-package
 (unless (package-installed-p 'use-package)
-	(package-refresh-contents)
-	(package-install 'use-package))
+  (package-refresh-contents)
+  (package-install 'use-package))
 
 ;; Display the time it took when starting up emacs.
 (defun display-startup-time ()
-	(message "Emacs loaded in %s with %d garbage collections."
-					 (format "%.2f seconds"
-									 (float-time
-									 (time-subtract after-init-time before-init-time)))
-					 gcs-done))
+  (message "Emacs loaded in %s with %d garbage collections."
+           (format "%.2f seconds"
+                   (float-time
+                   (time-subtract after-init-time before-init-time)))
+           gcs-done))
 (add-hook 'emacs-startup-hook #'display-startup-time)
 
 (defun reload-init-file ()
-	(interactive)
-	(load-file user-init-file)
-	(princ "init file reloaded"))
+  (interactive)
+  (load-file user-init-file)
+  (princ "init file reloaded"))
 
 (global-set-key (kbd "C-x C-l") 'reload-init-file)
 
 ;; Some startup time boosters
 (setq read-process-output-max (* 3 1024 1024)) ;; 3mb
 (setq gc-cons-threshold most-positive-fixnum ; 2^61 bytes
-			gc-cons-percentage 0.6)
+      gc-cons-percentage 0.6)
 
 (add-hook 'emacs-startup-hook
-	(lambda ()
-		(setq gc-cons-threshold 31457280 ; 32mb
-					gc-cons-percentage 0.1)))
+  (lambda ()
+    (setq gc-cons-threshold 31457280 ; 32mb
+          gc-cons-percentage 0.1)))
 
 (defun nro/defer-garbage-collection-h ()
-	(setq gc-cons-threshold most-positive-fixnum))
+  (setq gc-cons-threshold most-positive-fixnum))
 
 (defun nro/restore-garbage-collection-h ()
-	;; Defer it so that commands launched immediately after will enjoy the
-	;; benefits.
-	(run-at-time
-	 1 nil (lambda () (setq gc-cons-threshold 31457280))))
+  ;; Defer it so that commands launched immediately after will enjoy the
+  ;; benefits.
+  (run-at-time
+   1 nil (lambda () (setq gc-cons-threshold 31457280))))
 
 (add-hook 'minibuffer-setup-hook #'nro/defer-garbage-collection-h)
 (add-hook 'minibuffer-exit-hook #'nro/restore-garbage-collection-h)
@@ -54,61 +54,61 @@
 (setq file-name-handler-alist nil)
 
 (add-hook 'emacs-startup-hook
-	(lambda ()
-		(setq file-name-handler-alist nro--file-name-handler-alist)))
+  (lambda ()
+    (setq file-name-handler-alist nro--file-name-handler-alist)))
 
 (defun nro/lsp-ui-doc-show ()
-		(interactive)
-		(lsp-ui-doc-hide)
-		(lsp-ui-doc-show)
-		(lsp-ui-doc-show))
+    (interactive)
+    (lsp-ui-doc-hide)
+    (lsp-ui-doc-show)
+    (lsp-ui-doc-show))
 
 ;; Vim keybindings in emacs.
 (use-package evil
-	:config
-	(evil-mode 1)
-	(setq evil-vsplit-window-right t)
-	(setq evil-split-window-below t)
-	(evil-set-leader 'normal (kbd "SPC"))
+  :config
+  (evil-mode 1)
+  (setq evil-vsplit-window-right t)
+  (setq evil-split-window-below t)
+  (evil-set-leader 'normal (kbd "SPC"))
 
-	;; Such that there is no need to use the ESC-key.
-	(define-key evil-insert-state-map (kbd "C-j") 'evil-normal-state)
-	(define-key evil-insert-state-map (kbd "C-f") 'evil-delete-backward-char-and-join)
+  ;; Such that there is no need to use the ESC-key.
+  (define-key evil-insert-state-map (kbd "C-j") 'evil-normal-state)
+  (define-key evil-insert-state-map (kbd "C-f") 'evil-delete-backward-char-and-join)
 
-	;; Some keybindings for better window navigation
-	(evil-define-key 'normal 'global (kbd "<leader>wj") 'evil-window-bottom)
-	(evil-define-key 'normal 'global (kbd "<leader>wh") 'evil-window-left)
-	(evil-define-key 'normal 'global (kbd "<leader>wl") 'evil-window-right)
-	(evil-define-key 'normal 'global (kbd "<leader>wk") 'evil-window-up)
+  ;; Some keybindings for better window navigation
+  (evil-define-key 'normal 'global (kbd "<leader>wj") 'evil-window-bottom)
+  (evil-define-key 'normal 'global (kbd "<leader>wh") 'evil-window-left)
+  (evil-define-key 'normal 'global (kbd "<leader>wl") 'evil-window-right)
+  (evil-define-key 'normal 'global (kbd "<leader>wk") 'evil-window-up)
 
-	;; Other leader keybindings
-	(evil-define-key 'normal 'global (kbd "<leader>ff") 'find-file)
-	(evil-define-key 'normal 'global (kbd "<leader>sh") 'shell)
-	(evil-define-key 'normal 'global (kbd "<leader>fb") 'lsp-format-buffer)
-	(evil-define-key 'normal 'global (kbd "<leader>fr") 'lsp-format-region)
-	(evil-define-key 'normal 'global (kbd "<leader>dsh") 'nro/lsp-ui-doc-show)
-	(evil-define-key 'normal 'global (kbd "<leader>di") 'dired)
-	(evil-define-key 'normal 'global (kbd "<leader>ne") 'flyspell-goto-next-error)
-	(evil-define-key 'normal 'global (kbd "<leader>kb") 'kill-this-buffer)
+  ;; Other leader keybindings
+  (evil-define-key 'normal 'global (kbd "<leader>ff") 'find-file)
+  (evil-define-key 'normal 'global (kbd "<leader>sh") 'shell)
+  (evil-define-key 'normal 'global (kbd "<leader>fb") 'lsp-format-buffer)
+  (evil-define-key 'normal 'global (kbd "<leader>fr") 'lsp-format-region)
+  (evil-define-key 'normal 'global (kbd "<leader>dsh") 'nro/lsp-ui-doc-show)
+  (evil-define-key 'normal 'global (kbd "<leader>di") 'dired)
+  (evil-define-key 'normal 'global (kbd "<leader>ne") 'flyspell-goto-next-error)
+  (evil-define-key 'normal 'global (kbd "<leader>kb") 'kill-this-buffer)
 
-	;; Save a file.
-	(evil-define-key 'normal 'global (kbd "<leader>s") 'save-buffer)
+  ;; Save a file.
+  (evil-define-key 'normal 'global (kbd "<leader>s") 'save-buffer)
 
-	;; Use visual line motions even outside of visual-line-mode buffers
-	(evil-global-set-key 'motion "j" 'evil-next-visual-line)
-	(evil-global-set-key 'motion "k" 'evil-previous-visual-line)
+  ;; Use visual line motions even outside of visual-line-mode buffers
+  (evil-global-set-key 'motion "j" 'evil-next-visual-line)
+  (evil-global-set-key 'motion "k" 'evil-previous-visual-line)
 
-	(evil-set-initial-state 'messages-buffer-mode 'normal)
-	(evil-set-initial-state 'dashboard-mode 'normal)
-	(setq evil-insert-state-cursor 'hbar))
+  (evil-set-initial-state 'messages-buffer-mode 'normal)
+  (evil-set-initial-state 'dashboard-mode 'normal)
+  (setq evil-insert-state-cursor 'hbar))
 
 
 ;; Disable line numbers for some modes
 (dolist (mode '(org-mode-hook
-								term-mode-hook
-								shell-mode-hook
-								eshell-mode-hook))
-	(add-hook mode (lambda () (display-line-numbers-mode 0))))
+                term-mode-hook
+                shell-mode-hook
+                eshell-mode-hook))
+  (add-hook mode (lambda () (display-line-numbers-mode 0))))
 
 
 ;; Change indentation
@@ -143,10 +143,14 @@
 
 ;; Better scrolling
 (setq scroll-margin 0
-			scroll-conservatively 100000
-			scroll-preserve-screen-position 1)
+      scroll-conservatively 100000
+      scroll-preserve-screen-position 1)
 
 (setq mode-line-position '(line-number-mode ("%l")))
+
+;; Show empty scratch buffer and make the mode org mode
+(setq initial-scratch-message nil)
+(setq initial-major-mode 'org-mode)
 
 ;; Cleanup whitespaces
 (add-hook 'before-save-hook 'whitespace-cleanup)
@@ -155,27 +159,27 @@
 (global-set-key (kbd "C-x k") 'kill-this-buffer)
 
 (use-package helm
-	:ensure t
-	:config
-	(helm-mode 1)
-	(setq helm-autoresize-mode t)
-	(setq helm-buffer-max-length 40)
-	(global-set-key (kbd "M-x") #'helm-M-x)
-	(define-key helm-map (kbd "S-SPC") 'helm-toggle-visible-mark)
-	(define-key helm-find-files-map (kbd "C-k") 'helm-find-files-up-one-level))
+  :ensure t
+  :config
+  (helm-mode 1)
+  (setq helm-autoresize-mode t)
+  (setq helm-buffer-max-length 40)
+  (global-set-key (kbd "M-x") #'helm-M-x)
+  (define-key helm-map (kbd "S-SPC") 'helm-toggle-visible-mark)
+  (define-key helm-find-files-map (kbd "C-k") 'helm-find-files-up-one-level))
 
 ;; helm-projectile configuration
 (use-package helm-projectile
-	:bind (("C-S-P" . helm-projectile-switch-project)
-		 :map evil-normal-state-map
-		 ("C-p" . helm-projectile))
-	:ensure t)
+  :bind (("C-S-P" . helm-projectile-switch-project)
+     :map evil-normal-state-map
+     ("C-p" . helm-projectile))
+  :ensure t)
 
 ;; Enable rich presense on discord and some configuration for it.
 (use-package elcord
-	:config
-	(elcord-mode)
-	(setq elcord-use-major-mode-as-main-icon t))
+  :config
+  (elcord-mode)
+  (setq elcord-use-major-mode-as-main-icon t))
 
 ;; Disable the menubar
 (menu-bar-mode -1)
@@ -203,25 +207,27 @@
 
 
 (use-package modus-themes
-	:ensure
-	:init
-	;; Add all your customizations prior to loading the themes
-	(setq modus-themes-slanted-constructs t
-				modus-themes-bold-constructs nil
-				modus-themes-region 'no-extend
-				modus-themes-syntax 'yellow-comments-green-strings)
+  :ensure
+  :init
+  ;; Add all your customizations prior to loading the themes
+  (setq modus-themes-slanted-constructs nil
+        modus-themes-bold-constructs nil
+        modus-themes-region 'no-extend
+        modus-themes-syntax 'yellow-comments-green-strings
+        modus-themes-mode-line '3d
+        modus-themes-hl-line 'accented-background)
 
-	;; Load the theme files before enabling a theme
-	(modus-themes-load-themes)
-	:config
-	;; Load the theme of your choice:
-	(modus-themes-load-vivendi) ;; OR (modus-themes-load-operandi)
-	:bind ("<f5>" . modus-themes-toggle))
+  ;; Load the theme files before enabling a theme
+  (modus-themes-load-themes)
+  :config
+  ;; Load the theme of your choice:
+  (modus-themes-load-vivendi) ;; OR (modus-themes-load-operandi)
+  :bind ("<f5>" . modus-themes-toggle))
 
 
 ;; Add line number display
 (when (version<= "26.0.50" emacs-version )
-	(global-display-line-numbers-mode))
+  (global-display-line-numbers-mode))
 
 ;; Add line wrapping
 (global-visual-line-mode 1)
@@ -231,25 +237,26 @@
 (set-fringe-mode 10)
 
 ;; Set font
-(set-face-attribute 'default nil :font "JetBrains Mono Nerd Font" :height 130)
+(set-face-attribute 'default nil :font "JetBrains Mono Nerd Font" :height 140)
+(set-face-attribute 'variable-pitch nil :family "JetBrains Mono Nerd Font" :height 150)
 
 ;; Projectile configuration
 (use-package projectile
-	:config
-	(projectile-mode +1)
-	(define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
-	(define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map))
+  :config
+  (projectile-mode +1)
+  (define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
+  (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map))
 
 ;; Company configuration
 (use-package company
-	:ensure t
-	:config
-	(global-company-mode)
-	(setq company-idle-delay 0)
-	(setq company-selection-wrap-around t)
-	(define-key company-active-map [tab] 'company-complete)
-	(define-key company-active-map (kbd "C-n") 'company-select-next)
-	(define-key company-active-map (kbd "C-p") 'company-select-previous))
+  :ensure t
+  :config
+  (global-company-mode)
+  (setq company-idle-delay 0)
+  (setq company-selection-wrap-around t)
+  (define-key company-active-map [tab] 'company-complete)
+  (define-key company-active-map (kbd "C-n") 'company-select-next)
+  (define-key company-active-map (kbd "C-p") 'company-select-previous))
 
 (require 'lsp-mode)
 (add-hook 'go-mode-hook #'lsp-deferred)
@@ -261,34 +268,34 @@
 ;; Set up before-save hooks to format buffer and add/delete imports.
 ;; Make sure you don't have other gofmt/goimports hooks enabled.
 (defun lsp-go-install-save-hooks ()
-	(add-hook 'before-save-hook #'lsp-format-buffer t t)
-	(add-hook 'before-save-hook #'lsp-organize-imports t t))
+  (add-hook 'before-save-hook #'lsp-format-buffer t t)
+  (add-hook 'before-save-hook #'lsp-organize-imports t t))
 (add-hook 'go-mode-hook #'lsp-go-install-save-hooks)
 
 ;; Add support for rust
 (use-package rust-mode
-	:hook (rust-mode . lsp))
+  :hook (rust-mode . lsp))
 (setq rust-format-on-save t)
 
 ;; Add support for toml files which rust uses for configuration
 (use-package toml-mode)
 
 (use-package lsp-ui
-	:ensure t
-	:commands lsp-ui-mode
-	:config (setq lsp-ui-sideline-enable nil
-			lsp-ui-peek-enable t
-				lsp-ui-doc-enable nil
-				lsp-ui-flycheck-enable nil
-		lsp-ui-sideline-enable t
-				lsp-ui-imenu-enable t
-				lsp-ui-sideline-ignore-duplicate t))
+  :ensure t
+  :commands lsp-ui-mode
+  :config (setq lsp-ui-sideline-enable nil
+      lsp-ui-peek-enable t
+        lsp-ui-doc-enable nil
+        lsp-ui-flycheck-enable nil
+    lsp-ui-sideline-enable t
+        lsp-ui-imenu-enable t
+        lsp-ui-sideline-ignore-duplicate t))
 
 
 ;; Configuration for Go LSP support
 (defun lsp-go-install-save-hooks ()
-	(add-hook 'before-save-hook #'lsp-format-buffer t t)
-	(add-hook 'before-save-book #'lsp-organize-imports t t))
+  (add-hook 'before-save-hook #'lsp-format-buffer t t)
+  (add-hook 'before-save-book #'lsp-organize-imports t t))
 (add-hook 'go-mode-hook #'lsp-go-install-save-hooks)
 (add-hook 'go-mode-hook #'lsp-deferred)
 
@@ -308,6 +315,7 @@
 
 ;; Enable the usage of the system clipboard.
 (setq select-enable-clipboard t)
+(setq x-select-enable-clipboard t)
 
 ;; Make the max width of a line to be 80 characters.
 (setq fill-column 80)
@@ -344,50 +352,65 @@
 
 ;; For markdown editing
 (use-package markdown-mode
-	:ensure t
-	:commands (markdown-mode gfm-mode)
-	:mode (("README\\.md\\'" . gfm-mode)
-				 ("\\.md\\'" . markdown-mode)
-				 ("\\.markdown\\'" . markdown-mode))
-	:init (setq markdown-command "multimarkdown"))
+  :ensure t
+  :commands (markdown-mode gfm-mode)
+  :mode (("README\\.md\\'" . gfm-mode)
+         ("\\.md\\'" . markdown-mode)
+         ("\\.markdown\\'" . markdown-mode))
+  :init (setq markdown-command "multimarkdown"))
 
 ;; Git integration
 (use-package magit
-	:defer t
-	:bind (("C-x g" . magit-status)))
+  :defer t
+  :bind (("C-x g" . magit-status)))
 
 ;; So I don't have to type many things twice
 (use-package smartparens
-	:ensure t
-	:init
-	(smartparens-global-mode))
+  :ensure t
+  :init
+  (smartparens-global-mode))
 
 ;; Better looks by adding more icons
 (use-package all-the-icons)
 
 ;; Add the same icons when using dired
 (use-package all-the-icons-dired
-	:config
-	(add-hook 'dired-mode-hook #'all-the-icons-dired-mode)
-	(setq all-the-icons-dired-monochrome nil))
+  :config
+  (add-hook 'dired-mode-hook #'all-the-icons-dired-mode)
+  (setq all-the-icons-dired-monochrome nil))
+
+(add-hook 'text-mode-hook
+          'variable-pitch-mode)
+
+(customize-set-variable 'org-blank-before-new-entry
+                        '((heading . nil)
+                          (plain-list-item . nil)))
+(setq org-cycle-separator-lines 1)
+(setq org-adapt-indentation nil)
+(setq org-indent-indentation-per-level 1)
+
+;; Add spell checking
+(customize-set-variable 'ispell-program-name "aspell")
+(customize-set-variable 'ispell-extra-args '("--sug-mode=ultra"))
+(add-hook 'text-mode-hook 'flyspell-mode)
 
 (defun org-mode-setup ()
-	(org-indent-mode)
-	(variable-pitch-mode 1)
-	(auto-fill-mode 0)
-	(visual-line-mode 1)
-	(setq evil-auto-indent nil)
-	(setq org-hide-emphasis-markers t))
+  (org-indent-mode)
+  (variable-pitch-mode 1)
+  (auto-fill-mode 0)
+  (visual-line-mode 1)
+  (setq evil-auto-indent nil)
+  (setq org-hide-emphasis-markers t))
 
 (font-lock-add-keywords 'org-mode
-												'(("^ *\\([-]\\) "
-													(0 (prog1 () (compose-region (match-beginning 1) (match-end 1) "•"))))))
+                        '(("^ *\\([-]\\) "
+                          (0 (prog1 () (compose-region (match-beginning 1) (match-end 1) "•"))))))
 
 (require 'org-superstar)
 (add-hook 'org-mode-hook (lambda () (org-superstar-mode 1)))
 
 (use-package org
-	:hook (org-mode . org-mode-setup))
+  :hook (org-mode . org-mode-setup))
 
 (setq org-directory "~/docs/org")
 (setq org-agenda-files '("~/docs/org/todo.org" "~/docs/org/habits.org"))
@@ -397,27 +420,27 @@
 
 ;; Add more TODO keywords.
 (setq org-todo-keywords
-			(quote ((sequence "TODO(t)"
-												"IN_PROGRESS(i!)"
-												"DONE(d!)"
-												"CANCELLED(c!)"
-												"POSTPONED(p!)"
-												))))
+      (quote ((sequence "TODO(t)"
+                        "IN_PROGRESS(i!)"
+                        "DONE(d!)"
+                        "CANCELLED(c!)"
+                        "POSTPONED(p!)"
+                        ))))
 
 ;; Fast selection for todos
 (setq org-use-fast-todo-selection t)
 
 ;; Use evil mode in org-mode
 (use-package evil-org
-	:ensure t
-	:after (evil org)
-	:config
-	(add-hook 'org-mode-hook 'evil-org-mode)
-	(add-hook 'evil-org-mode-hook
-						(lambda ()
-							(evil-org-set-key-theme '(navigation insert textobjects additional calendar))))
-	(require 'evil-org-agenda)
-	(evil-org-agenda-set-keys))
+  :ensure t
+  :after (evil org)
+  :config
+  (add-hook 'org-mode-hook 'evil-org-mode)
+  (add-hook 'evil-org-mode-hook
+            (lambda ()
+              (evil-org-set-key-theme '(navigation insert textobjects additional calendar))))
+  (require 'evil-org-agenda)
+  (evil-org-agenda-set-keys))
 
 ;; Custom key bindings
 (global-set-key (kbd "C-/") 'comment-line)
@@ -439,25 +462,25 @@
 
 ;; Get the other file
 (add-hook 'c-mode-common-hook
-	(lambda()
-		(local-set-key  (kbd "C-c o") 'ff-find-other-file)))
+  (lambda()
+    (local-set-key  (kbd "C-c o") 'ff-find-other-file)))
 
 
 ;; Load a theme without all of questions
 (advice-add 'load-theme
-						:around
-						(lambda (fn theme &optional no-confirm no-enable)
-							(funcall fn theme t)))
+            :around
+            (lambda (fn theme &optional no-confirm no-enable)
+              (funcall fn theme t)))
 
 ;; Change to scratch buffer
 (global-set-key (kbd "<f12>")
-	(lambda()(interactive)
-		(switch-to-buffer (get-buffer-create "*scratch*"))))
+  (lambda()(interactive)
+    (switch-to-buffer (get-buffer-create "*scratch*"))))
 
 ;; Edit config file
 (defun nro/edit-config ()
-	(interactive)
-	(find-file (concat user-emacs-directory "init.el")))
+  (interactive)
+  (find-file (concat user-emacs-directory "init.el")))
 (global-set-key (kbd "<f11>") 'nro/edit-config)
 
 (custom-set-variables
@@ -466,11 +489,11 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-	 '("0e2a7e1e632dd38a8e0227d2227cb8849f877dd878afb8219cb6bcdd02068a52" "1623aa627fecd5877246f48199b8e2856647c99c6acdab506173f9bb8b0a41ac" default))
+   '("0e2a7e1e632dd38a8e0227d2227cb8849f877dd878afb8219cb6bcdd02068a52" "1623aa627fecd5877246f48199b8e2856647c99c6acdab506173f9bb8b0a41ac" default))
  '(helm-minibuffer-history-key "M-p")
  '(org-agenda-files '("~/docs/org/todo.org" "~/docs/org/habits.org"))
  '(package-selected-packages
-	 '(all-the-icons-dired toml-mode rust-mode org-superstar modus-themes elcord smartparens magit which-key helm-projectile projectile company lsp-ui lsp-mode go-mode use-package evil)))
+   '(all-the-icons-dired toml-mode rust-mode org-superstar modus-themes elcord smartparens magit which-key helm-projectile projectile company lsp-ui lsp-mode go-mode use-package evil)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
