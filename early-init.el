057c7e88963884d8841f615b861ba47f1820f051
nireo <eemil.lehtikangas1@gmail.com>
2022-04-02 17:08:22 +0300

    made startup a lot faster with a early-init.el file

---
+++ early-init.el
@@ -0,0 +1,9 @@
+;; -*- lexical-binding: t; -*-
+
+(setq frame-inhibit-implied-resize t)
+
+(push '(menu-bar-lines . 0) default-frame-alist)
+(push '(tool-bar-lines . 0) default-frame-alist)
+(push '(vertical-scroll-bars) default-frame-alist)
+(when (featurep 'ns)
+  (push '(ns-transparent-titlebar . t) default-frame-alist))
